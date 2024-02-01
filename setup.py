d7b1a4709d4d37d619ffea1706e817ff94399d6b
anatrini <134724389+anatrini@users.noreply.github.com>
2024-02-01 12:34:52 +0100

    Create setup.py
    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Performance-RNN-PyTorch',
+    version='1.0',
+    packages=find_packages(),
+    author='Alessandro Anatrini',
+    author_email='alessandro.anatrini@hfmt-hamburg.de',
+    description='PyTorch implementation of Magenta\'s popular Performance-RNN model',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'numpy==1.26.3',
+        'optuna==3.5.0',
+        'pandas==2.1.4',
+        'pretty_midi==0.2.10',
+        'tensorboard==2.15.1',
+        'tqdm==4.66.1'
+    ]
+)
